b993c8281da4489230da0e074d82463e1f469987
Bablu Ahmed <bablukpik@gmail.com>
2023-03-08 00:11:31 +0600

    main: example added for conditional statement

--- src/ui/index.js
+++ src/ui/index.js
@@ -7,25 +7,19 @@ const rules = [
   {
     action: 'module-1__feature-1__attribute-1', // action id
     subject: 'ABAC',
-    reason: 'You are not allowed',
+    reason: 'You are not allowed to perform this action',
   },
   {
-    action: 'module-1__feature-2__attribute-1',
+    action: 'module-1__feature-2__attribute-2',
     subject: 'ABAC',
-    reason: 'You are not allowed',
+    conditions: { published: false },
+    reason: 'You are not allowed to perform this action',
   },
 ];
 
 /**
 * Checks attributes based access control permissions
 * @param {string} attributeId - The attributeId is needed for checking access, i.e: module-2__feature-4__attribute-24
-* @param {object} attrs - The attrs is needed for condition property, i.e:
-    {
-      action: 'module-2__feature-4__attribute-24',
-      subject: 'ABAC',
-      conditions: { active: true },
-      reason: 'You are not allowed to access this resource',
-    }
 * @param {array} permissions - Permissions data initialization, i.e:
     [
       {
@@ -34,22 +28,39 @@ const rules = [
         reason: 'You are not allowed to access this resource',
       },
     ]
-  @returns {boolean} - true/false
+* @param {object} attrs - Conditional attributes are needed for adding conditions, i.e:
+    {
+      action: 'module-2__feature-4__attribute-24',
+      subject: 'ABAC',
+      conditions: { active: true },
+      reason: 'You are not allowed to access this resource',
+    }
+* @returns {boolean} - true/false
 */
 
 const hasAbility = (
   attributeId,
-  attrs = {},
   permissions = [],
+  attrs = {},
 ) => {
   // const rulesData = (permissions.length > 0 && permissions) || JSON.parse(localStorage?.getItem('userPermissions') || '[]');
+  // OR
   const rulesData = (permissions.length > 0 && permissions);
   const ability = createMongoAbility(rulesData);
   return ability.can(attributeId, subject('ABAC', attrs));
 };
 
-console.log('hasAbility', hasAbility('module-1__feature-1__attribute-1', {}, rules));
 
-// if(hasAbility('module-1__feature-2__attribute-1', {}, rules)) {
+// Example
+// if(hasAbility('module-1__feature-1__attribute-1', rules)) {
 //   <h1>Hello world!</h1> // component name here
 // }
+
+// Example 2
+// console.log('hasAbility', hasAbility('module-1__feature-1__attribute-1', rules)); // true
+console.log('hasAbility', hasAbility('module-1__feature-2__attribute-2',
+  rules,
+  {
+    published: true,
+  })
+);
