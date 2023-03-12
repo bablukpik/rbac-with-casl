b2d1fbdf41495fb1dca96762ee3e821d9abd473b
Bablu Ahmed <bablukpik@gmail.com>
2023-03-12 17:30:40 +0600

    main: new example added for abac

--- src/ui/index.js
+++ src/ui/index.js
@@ -15,53 +15,77 @@ const rules = [
     conditions: { published: false },
     reason: 'You are not allowed to perform this action',
   },
+  {
+    action: 'module-1__feature-3__attribute-3',
+    subject: 'ABAC',
+    conditions: { published: false },
+    fields: ['title', 'description'],
+    reason: 'You are not allowed to perform this action',
+  },
 ];
 
 /**
 * Checks attribute based access control permissions
-* @param {string} action - The action/permission is needed for checking access in the permission list, i.e:
-  whether module-2__feature-4__attribute-24 exists or not in the permissions list
 * @param {array} permissions - Permissions data initialization, i.e:
-    [
-      {
-        action: 'module-2__feature-4__attribute-24',
-        subject: 'ABAC',
-        reason: 'You are not allowed to access this resource',
-      },
-    ]
-* @param {object} attrs - Conditional attributes are needed for adding conditions, i.e:
+  [
     {
       action: 'module-2__feature-4__attribute-24',
       subject: 'ABAC',
-      conditions: { active: true },
       reason: 'You are not allowed to access this resource',
-    }
+    },
+  ]
+* @param {string} action - The action/permission is needed for checking access in the permission list, i.e:
+  whether module-2__feature-4__attribute-24 exists or not in the permissions list
+* @param {string} field - Conditional attributes are needed for adding conditions, i.e:
+  {
+    permissions: rules,
+    action: 'module-1__feature-3__attribute-3',
+    field: 'title',
+    conditions: { published: false },
+  }
+* @param {object} conditions - Conditional attributes are needed for adding conditions, i.e:
+  {
+    permissions: rules,
+    action: 'module-1__feature-2__attribute-2',
+    conditions: { published: true }
+  }
 * @returns {boolean} - true/false
 */
 
-const hasAbility = (
+const hasAbility = ({
   action,
+  subj = 'ABAC',
+  field = undefined,
+  conditions = {},
   permissions = [],
-  attrs = {},
-) => {
+}) => {
   // const rulesData = (permissions.length > 0 && permissions) || JSON.parse(localStorage?.getItem('userPermissions') || '[]');
   // OR
   const rulesData = (permissions.length > 0 && permissions);
   const ability = createMongoAbility(rulesData);
-  return ability.can(action, subject('ABAC', attrs));
+  return ability.can(action, subject(subj, conditions), field);
 };
 
 
 // Example
-// if(hasAbility('module-1__feature-1__attribute-1', rules)) {
+// console.log('hasAbility', hasAbility({ action: 'module-1__feature-1__attribute-1', permissions: rules })); // true
+// if(hasAbility({ action: 'module-1__feature-1__attribute-1', permissions: rules })) {
 //   <h1>Hello world!</h1> // component name here
 // }
 
 // Example 2
-// console.log('hasAbility', hasAbility('module-1__feature-1__attribute-1', rules)); // true
-console.log('hasAbility', hasAbility('module-1__feature-2__attribute-2',
-  rules,
-  {
-    published: false,
-  })
+// console.log('hasAbility', hasAbility({
+//   permissions: rules,
+//   action: 'module-1__feature-2__attribute-2',
+//   conditions: { published: true }
+// })
+// );
+
+// Example 3
+console.log('hasAbility', hasAbility({
+  permissions: rules,
+  action: 'module-1__feature-3__attribute-3',
+  field: 'title',
+  conditions: { published: false },
+})
 );
